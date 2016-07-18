9e79cbd851d8047ba4115a34be340d8ee4749fd2
Paul Rauwolf <paul.rauwolf@pearson.com>
2016-07-18 10:57:52 +0100

    Good is now a trait.

--- src/main/scala-2.11/Item/Good.scala
+++ src/main/scala-2.11/Item/Good.scala
@@ -4,11 +4,11 @@ package Item
  * A Good extends Item and represents a physical item worth 
  * some value
  */
-class Good(g:GoodType,q:Double)  extends Item {
+trait Good extends Item {
  
   //The type of Good
-  val Goodtype:GoodType = g
+  val goodType:GoodType
   
   //The quantity of the good possessed
-  var quantity:Double = q
+  var quantity:Double
 }
